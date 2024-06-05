bd61ac3b5c7319d426e0c970041fdf0851d6496a
ashlyn-m-farris <140285449+ashlyn-m-farris@users.noreply.github.com>
2024-06-05 16:42:49 -0400

    Update
    added folders, logic for threads (not complete)

--- OCR.py
+++ OCR.py
@@ -1,7 +1,14 @@
 import pytesseract
 import datefinder
 import re
+import json
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-text = pytesseract.image_to_string('screenshotImg/Jun3_24_1.jpg')
+text = pytesseract.image_to_string('images/PNA1/May30_24_16.jpg')
 print(text)
+
+handles = re.findall('@\w{4,15}', text)
+print(handles)
+
+if (len(handles)) > 1:
+    print("AN")
