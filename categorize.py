5bebf446bf7e59dfb56960e57c78a1ca874518bb
ashlyn-m-farris <140285449+ashlyn-m-farris@users.noreply.github.com>
2024-06-18 11:43:35 -0400

    Update categorize.py
    Fixed logic for categories

--- categorize.py
+++ categorize.py
@@ -1,25 +1,20 @@
 def categorize(dates, h_m, authors):  # sourcery skip: assign-if-exp
     #No author no dates
     if len(authors) == 0 and (len(dates) + len(h_m) == 0):
-        return("No author and timestamp info recieved.")
+        return("P1A1")
+
+    elif len(authors) == 0 and (len(dates) + len(h_m) == 1):
+        return('P1A1')
 
     #No authors at least 1 date
-    elif len(authors) == 0 and (len(dates) + len(h_m)) > 0:
-        print("There is no author information")
-        if len(dates) + len(h_m) > 1:
-            return('PN')
-        else:
-            return('P1')
+    elif len(authors) == 0 and (len(dates) + len(h_m)) > 1:
+        return('PNA1')
 
     #No dates at least 1 author
-    elif (len(dates) + len(h_m)) == 0 and len(authors) > 0:
-        print('There is no date information')
-        if len(authors) > 1:
-            return 'AN'
-        else:
-            return 'A1'
+    elif (len(dates) + len(h_m)) == 0 and len(authors) > 1:
+        return 'PNAN'
     
-    #At least 1 author and at least 1 date
+    #At least 1 author AND at least 1 date
     else:
         if len(dates) + len(h_m) > 1:
             post = 'PN'
